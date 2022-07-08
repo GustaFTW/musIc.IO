4c99174aa1a858e347144bd470d6842cc4a7850a
Gustavo Figueiredo <gustavofigueiredo002@gmail.com>
2022-07-08 19:53:36 -0300

    ThirdCommit

--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -1,41 +1,41 @@
-export function Keyboard() {
+export function Keyboard() { //grid grid-cols-5 grid-rows-8 mt-24 ml-auto mr-auto place-items-center max-w-md
     return (
-        <div className="grid grid-cols-5 grid-rows-8 mt-24 ml-auto mr-auto place-items-center max-w-md">
+        <div className="grid grid-cols-5 grid-rows-8 mt-24 ml-auto mr-auto place-items-center max-w-md bg-purple-400 border border-purple-400 rounded">
             <div>
-                <button className="text-4xl">C</button>
+                <button className="text-4xl rounded font-bold hover:bg-purple-800 transition-colors">C</button>
             </div>
             <div>
-                <button className="text-4xl">C#</button>
+                <button className="text-4xl rounded font-bold hover:bg-purple-800 transition-colors">C#</button>
             </div>
             <div>
-                <button className="text-4xl">D</button>
+                <button className="text-4xl rounded font-bold hover:bg-purple-800 transition-colors">D</button>
             </div>
             <div>
-                <button className="text-4xl">D#</button>
+                <button className="text-4xl rounded font-bold hover:bg-purple-800 transition-colors">D#</button>
             </div>
             <div>
-                <button className="text-4xl">E</button>
+                <button className="text-4xl rounded font-bold hover:bg-purple-800 transition-colors">E</button>
             </div>
             <div>
-                <button className="text-4xl">F</button>
+                <button className="text-4xl rounded font-bold hover:bg-purple-800 transition-colors">F</button>
             </div>
             <div>
-                <button className="text-4xl">F#</button>
+                <button className="text-4xl rounded font-bold hover:bg-purple-800 transition-colors">F#</button>
             </div>
             <div>
-                <button className="text-4xl border border-spacing-96 rounded">G</button>
+                <button className="text-4xl rounded font-bold hover:bg-purple-800 transition-colors">G</button>
             </div>
             <div>
-                <button className="text-4xl">G#</button>
+                <button className="text-4xl rounded font-bold hover:bg-purple-800 transition-colors">G#</button>
             </div>
             <div>
-                <button className="text-4xl">A</button>
+                <button className="text-4xl rounded font-bold hover:bg-purple-800 transition-colors">A</button>
             </div>
             <div>
-                <button className="text-4xl">A#</button>
+                <button className="text-4xl rounded font-bold hover:bg-purple-800 transition-colors">A#</button>
             </div>
             <div>
-                <button className="text-4xl">B</button>
+                <button className="text-4xl rounded font-bold hover:bg-purple-800 transition-colors">B</button>
             </div>
         </div>
     )
